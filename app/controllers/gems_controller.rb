61db27a1fd3685dbd9b3a5d650625f5c1a673ab8
Artem <artyezersky@gmail.com>
2019-10-27 12:40:31 +0200

    Revert "add reverse database save and get last page number"
    This reverts commit 9c4e3babd560b7b18103627020abe5095b4b18e9.

--- app/controllers/gems_controller.rb
+++ app/controllers/gems_controller.rb
@@ -1,5 +1,5 @@
 class GemsController < ApplicationController
-  before_action :find_gems, :gem_parser
+  before_action :find_gems
   require 'open-uri'
   require 'nokogiri'
   require 'json'
@@ -7,34 +7,19 @@ class GemsController < ApplicationController
 
   def gem_parser
     #@result = []
-    @page_nums = []
-    @last_page_num
-    url = 'https://rubysec.com/advisories'
-    html = open(url)
-    doc = Nokogiri::HTML(html)
-    doc.search('li a').each do |li|
-     a = li.to_s.gsub(/<[^>]*>/, "")
-     if a.scan(/\D/).empty?
-      @page_nums << a
-      @last_page_num = @page_nums.last.to_i
-     end
-    end
-    if Gemcheck.all[0] == nil
-      (1..@last_page_num).reverse_each do |page_number|
-        url = 'https://rubysec.com/advisories'
-        url += "?page=" + page_number.to_s
-        html = open(url)
-        doc = Nokogiri::HTML(html)
-        doc.search('tr').reverse_each do |tr|
-          cells = tr.search('th, td')
-          gem_date = cells[0].to_s.gsub(/<[^>]*>/, "").gsub(/\s+/, '')
-          gem_name = cells[1].to_s.gsub(/<[^>]*>/, "").gsub(/\s+/, '')
-          gem_title = cells[2].to_s.gsub(/<[^>]*>/, "").gsub(/^\s+/, '')
-          gem_cve = cells[3].to_s.gsub(/<[^>]*>/, "").gsub(/\s+/, '')
-          Gemcheck.create(date: gem_date, rubygem: gem_name, title: gem_title, cve: gem_cve)
-          Gemnum.create(gemnumber: Gemcheck.all.length)
-          #@result << gem_name
-        end
+    (1..15).each do |page_number|
+      url = 'https://rubysec.com/advisories'
+      url += "?page=" + page_number.to_s
+      html = open(url)
+      doc = Nokogiri::HTML(html)
+      doc.search('tr').each do |tr|
+        cells = tr.search('th, td')
+        gem_date = cells[0].to_s.gsub(/<[^>]*>/, "").gsub(/\s+/, '')
+        gem_name = cells[1].to_s.gsub(/<[^>]*>/, "").gsub(/\s+/, '')
+        gem_title = cells[2].to_s.gsub(/<[^>]*>/, "").gsub(/^\s+/, '')
+        gem_cve = cells[3].to_s.gsub(/<[^>]*>/, "").gsub(/\s+/, '')
+        Gemcheck.create(date: gem_date, rubygem: gem_name, title: gem_title, cve: gem_cve)
+        #@result << gem_name
       end
     end
   end
--- app/worker/hard_worker.rb
+++ app/worker/hard_worker.rb
@@ -19,23 +19,6 @@ class HardWorker
     end
     if @result.length != Gemnum.all.last.gemnumber
       Gemnum.create(gemnumber: @result.length)
-      (1..15).each do |page_number|
-        url = 'https://rubysec.com/advisories'
-        url += "?page=" + page_number.to_s
-        html = open(url)
-        doc = Nokogiri::HTML(html)
-        doc.search('tr').each do |tr|
-          cells = tr.search('th, td')
-          gem_date = cells[0].to_s.gsub(/<[^>]*>/, "").gsub(/\s+/, '')
-          gem_name = cells[1].to_s.gsub(/<[^>]*>/, "").gsub(/\s+/, '')
-          gem_title = cells[2].to_s.gsub(/<[^>]*>/, "").gsub(/^\s+/, '')
-          gem_cve = cells[3].to_s.gsub(/<[^>]*>/, "").gsub(/\s+/, '')
-          Gemcheck.create(date: gem_date, rubygem: gem_name, title: gem_title, cve: gem_cve)
-          p "NEW GEM RECORDER"
-          break
-        end
-        break
-      end
     else
       p "No new gems"
     end
